d9c88f9392d0caeabef0dd56fa6d453d9612a319
jvlokao <39078882+jvlokao@users.noreply.github.com>
2019-06-30 03:32:31 -0300

    main tira

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main
 {
 	public static void main(String args[])
 	{
-		OrdenacaoTopologica ord = new OrdenacaoTopologica();
+//		OrdenacaoTopologica ord = new OrdenacaoTopologica();
+//		
+//		String nomeEntrada = "teste.txt";
+//		
+//		ord.realizaLeitura(nomeEntrada);
+//		
+//		if(!ord.executa())
+//			System.out.println("O conjunto nao é parcialmente ordenado.");
+//		else
+//			System.out.println("O conjunto é parcialmente ordenado.");
 		
-		String nomeEntrada = "teste.txt";
+		File entradaFile = new File("src/entrada.txt");
 		
-		ord.realizaLeitura(nomeEntrada);
+		try {
+			Scanner entradaScanner = new Scanner(entradaFile);
+			
+			String[] nomes = new String[12];
+			
+			for(int i = 0; i < 13; i++) {
+				nomes[i] = entradaScanner.next();
+			}
+				
+			
+			for(int i = 0;i<nomes.length;i++) {
+				
+				System.out.println(nomes[i]);
+				
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 		
-		if(!ord.executa())
-			System.out.println("O conjunto nao é parcialmente ordenado.");
-		else
-			System.out.println("O conjunto é parcialmente ordenado.");
+		
+		
+		
+		
 	}
 }
